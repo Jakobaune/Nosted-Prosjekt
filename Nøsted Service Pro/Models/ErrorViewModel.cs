17e9285d2789048b963f84a99c40286da6676e67
mathiasirgemo <mathiasirgemo1@gmail.com>
2023-09-14 13:25:56 +0200

    lagt til

---
+++ Nøsted Service Pro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nøsted_Service_Pro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
