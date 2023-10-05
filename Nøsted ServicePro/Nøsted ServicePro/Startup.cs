965260f14005c37f3d87273f4ba3f85244779138
Sanderhalvors1 <sanderhalvorsen@icloud.com>
2023-10-05 15:31:15 +0200

    Sikkerhet

--- Nøsted ServicePro/Nøsted ServicePro/Startup.cs
+++ Nøsted ServicePro/Nøsted ServicePro/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Nøsted_ServicePro.Controllers.ServiceRegistrering;
+using System.Text.Json.Serialization;
 
 namespace Nøsted_ServicePro
 {
@@ -6,7 +8,30 @@ namespace Nøsted_ServicePro
     {
         public void Configure(IApplicationBuilder app)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+
+
+                context.Response.Headers.Add("X-Frame-Options", "DENY");
+
+
+                context.Response.Headers.Add("Referrer-Policy", "no - referrer");
+
+
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+
+
+                context.Response.Headers.Add("Content-Security",
+                "default - src 'self';  " +
+                "img-src 'self'; " +
+                "font-src 'self' ;" +
+                "style-src 'self' ;" +
+                "script-src 'self';" +
+                "connect-src 'self';");
+                await next();
+            });
         }
     }
 }
