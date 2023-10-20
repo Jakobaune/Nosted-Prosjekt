32d7d8e881f512787c0efbca2629c50b859a25d7
Sindreln <sindreln@uia.no>
2023-10-20 19:28:37 +0200

    Lagde sjekkliste

---
+++ NostedServicePro/Controllers/SjekklisteController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Loginnosted.Models; // Sørg for å inkludere riktig namespace for din Sjekkliste-modell
+using Microsoft.EntityFrameworkCore;
+using Loginnosted.Data;
+
+namespace DinProsjekt.Controllers
+{
+    public class SjekklisteController : Controller
+    {
+        private readonly ServiceProDbContex _context; // Bytt ut "YourDbContext" med din faktiske databasekontekstklasse
+
+        public SjekklisteController(ServiceProDbContex context)
+        {
+            _context = context;
+        }
+
+        // Viser listen over sjekklister
+        public IActionResult Sjekking()
+        {
+
+            var sjekklister = _context.Sjekkliste.ToList(); // Hent sjekklister fra databasen
+            var førsteSjekkliste = sjekklister.FirstOrDefault(); // Velg den første sjekklisten (eller en annen etter dine behov)
+            return View(førsteSjekkliste);
+        }
+
+
+        // Viser detaljer for en spesifikk sjekkliste
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sjekkliste = _context.Sjekkliste.FirstOrDefault(m => m.Id == id);
+            if (sjekkliste == null)
+            {
+                return NotFound();
+            }
+
+            return View(sjekkliste);
+        }
+
+        // Viser skjemaet for å opprette en ny sjekkliste
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Lagrer en ny sjekkliste i databasen
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Sjekkliste sjekkliste)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sjekkliste);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Sjekking));
+            }
+            return View(sjekkliste);
+        }
+
+        // Viser skjemaet for å redigere en eksisterende sjekkliste
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sjekkliste = _context.Sjekkliste.FirstOrDefault(m => m.Id == id);
+            if (sjekkliste == null)
+            {
+                return NotFound();
+            }
+
+            return View(sjekkliste);
+        }
+
+        // Lagrer endringene for en eksisterende sjekkliste i databasen
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Sjekkliste sjekkliste)
+        {
+            if (id != sjekkliste.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sjekkliste);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SjekklisteExists(sjekkliste.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Sjekking));
+            }
+            return View(sjekkliste);
+        }
+
+        // Viser skjemaet for å slette en eksisterende sjekkliste
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sjekkliste = _context.Sjekkliste.FirstOrDefault(m => m.Id == id);
+            if (sjekkliste == null)
+            {
+                return NotFound();
+            }
+
+            return View(sjekkliste);
+        }
+
+        // Sletter en eksisterende sjekkliste fra databasen
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var sjekkliste = _context.Sjekkliste.Find(id);
+            _context.Sjekkliste.Remove(sjekkliste);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Sjekking));
+        }
+
+        // Hjelpefunksjon for å sjekke om en sjekkliste eksisterer i databasen
+        private bool SjekklisteExists(int id)
+        {
+            return _context.Sjekkliste.Any(e => e.Id == id);
+        }
+    }
+}
--- NostedServicePro/Models/Sjekkliste.cs
+++ NostedServicePro/Models/Sjekkliste.cs
@@ -5,36 +5,28 @@ public class Sjekkliste
     [Key]
     public int Id { get; set; }
 
-    public Status ClutchLameller { get; set; }
-    public Status Bremser { get; set; }
-    public Status LagerTrommel { get; set; }
-    public Status PTOOpplagring { get; set; }
-    public Status Kjedestrammer { get; set; }
-    public Status Wire { get; set; }
-    public Status PlnlonLager { get; set; }
-    public Status KilleKjedehjul { get; set; }
-    public Status HydraulikkSylinderLekkasje { get; set; }
-    public Status SlangerSkaderLekkasje { get; set; }
-    public Status TestHydraulikkblokk { get; set; }
-    public Status SkiftOljeTank { get; set; }
-    public Status SkiftOljeGirboks { get; set; }
-    public Status RingsyllingerSkiftTelnlnger { get; set; }
-    public Status BremsesylingerSkiftTelninger { get; set; }
-    public Status LedningsnettVinsj { get; set; }
-    public Status SjekkTestRadio { get; set; }
-    public Status SjekkTestKnappekasse { get; set; }
-    public Status XXBar { get; set; }
-    public Status TestVinsjAlleFunksjoner { get; set; }
-    public Status TrekkraftKn { get; set; }
-    public Status BremsekraftKn { get; set; }
-
-    public enum Status
-    {
-        [Display(Name = "OK")]
-        OK,
-        [Display(Name = "Bør skiftes")]
-        BørSkiftes,
-        [Display(Name = "Defekt")]
-        Defekt
-    }
+    public String ClutchLameller { get; set; }
+    public String Bremser { get; set; }
+    public String LagerTrommel { get; set; }
+    public String PTOOpplagring { get; set; }
+    public String Kjedestrammer { get; set; }
+    public String Wire { get; set; }
+    public String PlnlonLager { get; set; }
+    public String KilleKjedehjul { get; set; }
+    public String HydraulikkSylinderLekkasje { get; set; }
+    public String SlangerSkaderLekkasje { get; set; }
+    public String TestHydraulikkblokk { get; set; }
+    public String SkiftOljeTank { get; set; }
+    public String SkiftOljeGirboks { get; set; }
+    public String RingsyllingerSkiftTelnlnger { get; set; }
+    public String BremsesylingerSkiftTelninger { get; set; }
+    public String LedningsnettVinsj { get; set; }
+    public String SjekkTestRadio { get; set; }
+    public String SjekkTestKnappekasse { get; set; }
+    public String XXBar { get; set; }
+    public String TestVinsjAlleFunksjoner { get; set; }
+    public String TrekkraftKn { get; set; }
+    public String BremsekraftKn { get; set; }
+ 
+    
 }
