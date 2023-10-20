866ffa8327c00483332daf51cb651b1524a58f64
Sindreln <sindreln@uia.no>
2023-10-20 17:23:32 +0200

    La til model sjekkliste

--- NostedServicePro/Models/Sjekkliste.cs
+++ NostedServicePro/Models/Sjekkliste.cs
@@ -1,22 +1,40 @@
-﻿using System.Collections;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Loginnosted.Models
+public class Sjekkliste
 {
-    public class Sjekkliste : IEnumerable<Sjekkliste>
+    [Key]
+    public int Id { get; set; }
+
+    public Status ClutchLameller { get; set; }
+    public Status Bremser { get; set; }
+    public Status LagerTrommel { get; set; }
+    public Status PTOOpplagring { get; set; }
+    public Status Kjedestrammer { get; set; }
+    public Status Wire { get; set; }
+    public Status PlnlonLager { get; set; }
+    public Status KilleKjedehjul { get; set; }
+    public Status HydraulikkSylinderLekkasje { get; set; }
+    public Status SlangerSkaderLekkasje { get; set; }
+    public Status TestHydraulikkblokk { get; set; }
+    public Status SkiftOljeTank { get; set; }
+    public Status SkiftOljeGirboks { get; set; }
+    public Status RingsyllingerSkiftTelnlnger { get; set; }
+    public Status BremsesylingerSkiftTelninger { get; set; }
+    public Status LedningsnettVinsj { get; set; }
+    public Status SjekkTestRadio { get; set; }
+    public Status SjekkTestKnappekasse { get; set; }
+    public Status XXBar { get; set; }
+    public Status TestVinsjAlleFunksjoner { get; set; }
+    public Status TrekkraftKn { get; set; }
+    public Status BremsekraftKn { get; set; }
+
+    public enum Status
     {
-        public int Id { get; set; }
-        public string KundeNavn { get; set; }
-        public List<SjekklisteElement> SjekklisteElement { get; set; }
-
-        public IEnumerator<Sjekkliste> GetEnumerator()
-        {
-            return this.SjekklisteElement.Cast<Sjekkliste>().GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+        [Display(Name = "OK")]
+        OK,
+        [Display(Name = "Bør skiftes")]
+        BørSkiftes,
+        [Display(Name = "Defekt")]
+        Defekt
     }
-
 }
--- NostedServicePro/Models/SjekklisteViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Loginnosted.Models
-{
-    public class SjekklisteViewModel
-    {
-        public List<SjekklisteElement> Sjekkpunkter { get; set; }
-        public Dictionary<string, string> SelectedSjekkpunkter { get; set; }
-    }
-}
